1854ed88c8527cf1724c91be1b7ff879a5880b5a
mahiki <mahiki@users.noreply.github.com>
2022-07-19 22:29:49 -0700

    start #3, add basic pytest tests

--- tests/test_desertislandutils.py
+++ tests/test_desertislandutils.py
@@ -2,4 +2,5 @@ from desertislandutils import __version__
 
 
 def test_version():
+    # I don't know why I'm testing this but at least the import worked in pytest
     assert __version__ == '0.1.0'
