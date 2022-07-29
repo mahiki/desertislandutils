69c5a0c089e2606eafa09ff226fe71e99895618d
mahiki <mahiki@users.noreply.github.com>
2022-07-29 16:03:54 -0700

    ready to push release branch and github action workflow

--- bump_version.sh
+++ bump_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cat << EOF
 REMINDER: You need to manually bump the version numbers in these locations.
-tests/test_desertislandutils.py
-desertislandutils/__init__.py
+__init__.py
 pyproject.toml
+tests/test_desertislandutils.py
 EOF
